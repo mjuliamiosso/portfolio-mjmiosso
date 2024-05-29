21d77e37f4e4ff260d62fa883450b30c6a3a1863
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-05-29 12:12:01 -0300

    adição das imagens de projetos

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Bem-vindo ao meu portfólio! Aqui você encontrará uma breve descrição sobre 
 Criei e codei meu portfolio usando o Figma e React, combinando meu amor por design com minhas habilidades em desenvolvimento Front-End.
 
 - [Figma](https://www.figma.com/design/DFALoDilR4aTVW2Z5ak9Pe/Maria-J%C3%BAlia-Miosso---Portfolio?node-id=0%3A1&t=sK7MTETSsmw5UUWp-1)
-- Deploy
+- [Deploy](https://portfolio-mjmiosso.vercel.app/)
 
 ## 💻 Tecnologias Utilizadas
 
--- src/views/Projects/Projects.jsx
+++ src/views/Projects/Projects.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Project from '../../components/Project'
 import './index.scss'
 import DncWeatherImg from '../../assets/dnc-weather.png'
+import SoonImg from '../../assets/em-breve.png'
+import SoonImgWide from '../../assets/em-breve-wide.png'
 import ProjectDescription from '../../components/ProjectDescription'
 import { Fade } from "react-awesome-reveal";
 
@@ -18,18 +20,18 @@ const Projects = () => {
               <ProjectDescription title='DncWeather' text='Desafio feito pela Escola DNC onde temos que desenvolver uma tela funcional com consumo de API.' github='https://github.com/mjuliamiosso/desafio-02-api?tab=readme-ov-file' deploy='https://mjuliamiosso.github.io/desafio-02-api/'></ProjectDescription>
             </div>
             <div className='project-container'>
-              <Project image={DncWeatherImg}></Project>
-              <ProjectDescription title='DncWeather' text='Desafio feito pela Escola DNC onde temos que desenvolver uma tela funcional com consumo de API.' github='https://github.com/mjuliamiosso/desafio-02-api?tab=readme-ov-file' deploy='https://mjuliamiosso.github.io/desafio-02-api/'></ProjectDescription>
+              <Project image={SoonImgWide}></Project>
+              <ProjectDescription title='---' text='---' ></ProjectDescription>
             </div>
           </div>
           <div className='projects-container-left'>
             <div className='project-container'>
-              <Project image={DncWeatherImg}></Project>
-              <ProjectDescription title='DncWeather' text='Desafio feito pela Escola DNC onde temos que desenvolver uma tela funcional com consumo de API.' github='https://github.com/mjuliamiosso/desafio-02-api?tab=readme-ov-file' deploy='https://mjuliamiosso.github.io/desafio-02-api/'></ProjectDescription>
+              <Project image={SoonImgWide}></Project>
+              <ProjectDescription title='---' text='---' ></ProjectDescription>
             </div>
             <div className='project-container'>
-              <Project image={DncWeatherImg}></Project>
-              <ProjectDescription title='DncWeather' text='Desafio feito pela Escola DNC onde temos que desenvolver uma tela funcional com consumo de API.' github='https://github.com/mjuliamiosso/desafio-02-api?tab=readme-ov-file' deploy='https://mjuliamiosso.github.io/desafio-02-api/'></ProjectDescription>
+              <Project image={SoonImg}></Project>
+              <ProjectDescription title='---' text='---' ></ProjectDescription>
             </div>
           </div>
         </Fade>
