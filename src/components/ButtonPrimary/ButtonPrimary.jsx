66523a31ebf06893ef651466183decd7de23bd4c
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-10-21 10:52:17 -0300

    styles(buttons): styles update

---
+++ src/components/ButtonPrimary/ButtonPrimary.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './ButtonPrimary.module.scss'
+
+const ButtonPrimary = ({text}) => {
+  return (
+    <div>
+        <button className={styles.buttonPrimary}>{text}</button>
+    </div>
+  )
+}
+
+export default ButtonPrimary
