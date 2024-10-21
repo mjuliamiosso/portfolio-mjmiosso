b178db4b610422b3e1f3b5e4a92c68f0b13684c8
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-10-21 10:57:38 -0300

    styles(navbar): styles update

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import classNames from 'classnames';
+import styles from './Navbar.module.scss'
+import '../../sass/_mixins.scss'
+import { IoMenu, IoMoon } from "react-icons/io5";
+
+const Navbar = () => {
+
+  return (
+    <div className='colored-bg'>
+      <nav className='container'>
+        <a
+          className={styles.logo}
+          href="#header">
+          MJMIOSSO
+        </a>
+        <div className={styles.items}>
+          <div className={styles.links}>
+            <a
+              href="#aboutme">
+              Sobre mim
+            </a>
+            <a
+              href="#portfolio">
+              Porfólio
+            </a>
+            <a
+              href="#contact">
+              Contato
+            </a>
+          </div>
+          <div className={styles.icons}>
+            <IoMoon
+              className={styles.icon}
+            />
+            <IoMenu
+              className={classNames(styles.icon, 'menu')}
+            />
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
